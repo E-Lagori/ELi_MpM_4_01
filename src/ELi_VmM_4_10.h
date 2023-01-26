30d436d5108d46b3c16fd9115fee9aaff2330ee4
E-LAGORi Prototype your dreams <contact@e-lagori.com>
2023-01-26 14:44:17 +0530

    Update ELi_VmM_4_10.h
    Header file corrected

--- src/ELi_VmM_4_10.h
+++ src/ELi_VmM_4_10.h
@@ -1,39 +1,32 @@
-#ifndef VmM_ESP32D_4_10_H
-#define VmM_ESP32D_4_10_H
+/*
+ * Redirects the header to appropriate version of microphone header file
+ *
+ * Copyright (C) 2022  E-Lagori https://github.com/E-Lagori/ELi_VmM_4_10
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file contains the code for ELi_MCM_4_00 library.
+ *
+ */
 
-#include <ELi_McM_4_10.h>
-#include<stdint.h>
+#ifndef ELi_VmM_4_10_H
+#define ELi_VmM_4_10_H
 
+#include<ELi_McM_4_00.h>
 
-class VmM_4_10{
-    private:
-        unsigned addr,d_bit;
-        int VmM_RT = 10000,VmM_RC = 1000,VmM_RW = 40;
-        float VmM_NGain_1dB,VmM_NGain_dB,VmM_NGain,VmM_Gain_1dB,VmM_RWB,VmM_RAW,D2;
-        uint8_t txbuffer[8],D_1,D_2,ND_1,ND_2;
-        float Gain_dB_val,NGain_dB_val,Max_dB_val=28.43,Min_dB_val=-40.46;
-        int VmM_read_data;
-        int VmM_sendi2cdata();
-        int VmM_received_data();
-        int VmM_Command16();
-        float VmM_GainCal(uint8_t,uint8_t);
-        int VmM_Present_val(int ,bool);
-        int VmM_Write(bool, int);
-        int VmM_StoreValue (bool);
-        int VmM_RestoreValue (bool);
-        int VmM_chgBoardadd(int);
-        int VmM_check_i2c(int);
-        int VmM_receivei2cdata();
-    public:   
-        VmM_4_00(int);
-        float setGain(float);
-        void storeGain ();
-        void restoreGain ();
-        float incGain(float);
-        float decGain(float);
-        float amplify(float);
-        float attenuate(float);
-        float presentGain();
-};
-
+#ifdef ESP32
+  #include "esp32/VmM_ESP32D_4_10.h"
+#else 
+  #error "Controller not supported"
+#endif
 #endif
